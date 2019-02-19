55b804393f744e758f654a763961baad0c1d48d4
Stephen Fox <stevethecoder34@gmail.com>
2019-02-18 23:02:52 -0500

    Justification for giving Shrinkage a 'input_dimensions' property:
    > The thought is that channel depth doesn't change the output dimensions for CNNs, and that's
      attribute we're concerned with in the convulotional case...
      * Linear layers only have two dimensions, so it's a huge deal there.
      * RNNs do linear things over 'timesteps', so it's a big deal there.
      * Residual/identity/skip-connections in CNNs need this.
    
    > __It's decided__. The attribute stays

--- morph/nn/shrink.py
+++ morph/nn/shrink.py
@@ -1,8 +1,8 @@
 from morph.layers.sparse import percent_waste
 from morph.utils import check, round
-from .utils import in_dim, out_dim
+from .resizing import Resizing
+from .utils import in_dim, out_dim, group_layers_by_algo
 from .widen import resize_layers
-from .utils import group_layers_by_algo
 from ._types import type_name
 
 from typing import List
@@ -11,17 +11,45 @@ import torch.nn as nn
 
 
 class Shrinkage:
+    """
+    An intermediary for the "Shrink" step of the three step Morphing algorithm.
+    Rather than have all of the state be free in the small scope of a mega-function,
+    these abstractions ease the way of implementing the shrinking and prune of the
+    network.
+    * Given that we have access to the total count of nodes, and how wasteful a layer was
+      we can deduce any necessary changes once given a new input dimension
+    * We expect input dimensions to change to accomodate the trimmed down earlier layers,
+      but we want an expansion further along to allow the opening of bottlenecks in the architecture
+    """
 
-    def __init__(self, initial_parameters: int, waste_percentage: float):
+    def __init__(self, input_dimension: int, initial_parameters: int,
+                 waste_percentage: float):
+        self.input_dimension = input_dimension  # TODO: is this relevant in any non-Linear case?
         self.initial_parameters = initial_parameters
         self.waste_percentage = waste_percentage
+        self.reduced_parameters = Shrinkage.reduce_parameters(initial_parameters,
+                                                              waste_percentage)
 
-    @property
-    def reduced_parameters(self):
-        percent_keep = (1. - self.waste_percentage)
-        unrounded_params_to_keep = percent_keep * self.initial_parameters
+    @staticmethod
+    def reduce_parameters(initial_parameters: int, waste: float) -> int:
+        """Calculates the new, smaller, number of paratemers that this instance encapsulates"""
+        percent_keep = (1. - waste)
+        unrounded_params_to_keep = percent_keep * initial_parameters
         # round digital up to the nearest integer
         return round(unrounded_params_to_keep)
+
+
+def shrink_to_resize(shrinkage: Shrinkage, new_input_dimension: int) -> Resizing:
+    """Given the `new_input_dimension`, calculate a reshaping/resizing for the parameters
+    of the supplied `shrinkage`.
+    We round up the new output dimension, generously allowing for opening bottlenecks.
+    Iteratively, any waste introduced is pruned hereafter. (Needs proof/unit test)
+    """
+    new_output_dimension = round(shrinkage.reduced_parameters / new_input_dimension)
+    return Resizing(new_input_dimension, new_output_dimension)
+
+
+#################### prove of a good implementation ####################
 
 
 def uniform_prune(net: nn.Module) -> nn.Module:
@@ -35,15 +63,16 @@ def uniform_prune(net: nn.Module) -> nn.Module:
 def shrink_layer(layer: nn.Module) -> Shrinkage:
     waste = percent_waste(layer)
     parameter_count = layer.weight.numel()  # the count is already tracked for us
-    return Shrinkage(parameter_count, waste)
+    return Shrinkage(in_dim(layer), parameter_count, waste)
 
 
-def fit_layer_sizes(layer_sizes: List[Shrinkage]) -> List[Shrinkage]:
-
+def fit_layer_sizes(layer_sizes: List[Shrinkage]) -> List[Resizing]:
+    # TODO: where's the invocation site for shrink_to_resize
     pass
 
 
-def transform(original_layer: nn.Module, new_shape: Shrinkage) -> nn.Module:
+def transform(original_layer: nn.Module, new_shape: Resizing) -> nn.Module:
+    # TODO: this might just be utils.redo_layer, without the primitive obsession
     pass
 
 
